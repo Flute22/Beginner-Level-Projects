084bcb949bef35237524f0c6255ea61cf84cbd82
Khushal <shimarsabb@gmail.com>
2024-11-02 22:18:07 +0530

    Update main.js

--- Calculator/main.js
+++ Calculator/main.js
@@ -2,9 +2,6 @@ const audio = new Audio("./assets/click4.mp3")
 const display = document.querySelector('.display');
 const btns = document.querySelectorAll('.btn');
 
-const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const operators = ['+', '-', '*', '/', '%']
-
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
         audio.play();
